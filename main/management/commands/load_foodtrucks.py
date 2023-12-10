8448875226d86cb209afe5139d4f95cd9c15f7d1
pf-github-pl <pf.github@gmail.com>
2023-12-10 20:52:19 +0100

    create Django command for loading data Food Truck data from csv file using pandas library

---
+++ main/management/commands/load_foodtrucks.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from django.core.management.base import BaseCommand
+from django.core.management.base import CommandError
+
+from main.models import FoodTruck
+
+
+class Command(BaseCommand):
+    help = 'Loads data from food-truck-data.csv into FoodTruck model'
+
+    def handle(self, *args, **options):
+        csv_file_path = 'main/food-truck-data.csv'
+        try:
+            data = pd.read_csv(csv_file_path)
+            food_trucks = data[['Applicant', 'FacilityType', 'Address', 'Latitude', 'Longitude', 'FoodItems', 'Status']]
+            food_trucks.columns = [
+                'applicant', 'facility_type', 'address', 'latitude', 'longitude', 'food_items', 'status'
+            ]
+            print(food_trucks.head())
+            for _, row in food_trucks.iterrows():
+                FoodTruck.objects.create(**row.to_dict())
+        except Exception as e:
+            raise CommandError(f'Error occurred while processing CSV file: {e}') from e
+
+        self.stdout.write(self.style.SUCCESS('Successfully loaded data into FoodTruck model'))
