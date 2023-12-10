3724763d8f5b6f14ac3409c7f01a27e69bf4f53c
pf-github-pl <pf.github@gmail.com>
2023-12-10 20:07:36 +0100

    Create FoodTruck model and add migration

--- main/models.py
+++ main/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Timestamped(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+
+class FoodTruck(Timestamped):
+    class Meta:
+        verbose_name_plural = "Food Trucks"
+
+    applicant = models.CharField(max_length=100)
+    facility_type = models.CharField(max_length=50, blank=True, null=True)
+    address = models.CharField(max_length=500, blank=True, null=True)
+    latitude = models.DecimalField(max_digits=10, decimal_places=7)
+    longitude = models.DecimalField(max_digits=10, decimal_places=7)
+    food_items = models.CharField(max_length=1000, blank=True, null=True)
+    dayshours = models.CharField(max_length=100, blank=True, null=True)
+    status = models.CharField(max_length=100, blank=True, null=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.applicant} {self.status} {self.active}"
