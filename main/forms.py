7efb27b33b538a2c62ac0a426c3756d7ac615f2d
pf-github-pl <pf.github@gmail.com>
2023-12-10 23:50:29 +0100

    create location form

---
+++ main/forms.py
@@ -0,0 +1,15 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from django import forms
+
+
+class LocationForm(forms.Form):
+
+    latitude = forms.CharField()
+    longitude = forms.CharField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = "post"
+        self.helper.add_input(Submit('submit', 'Submit', css_class="btn btn-primary rounded-pill w-100"))
