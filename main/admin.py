50b7f80a49a5a10b5171f214d8055bfe1dfa08b2
pf-github-pl <pf.github@gmail.com>
2023-12-10 20:50:22 +0100

    create admin for Food Truck model

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from main.models import FoodTruck
+
+
+@admin.register(FoodTruck)
+class FoodTruckAdmin(admin.ModelAdmin):
+    list_display = ["applicant", "facility_type", "address", "latitude", "longitude", "food_items", "status", "active"]
+    list_filter = ["status", "active"]
+    search_fields = ["applicant", "facility_type", "address", "food_items"]
