8a05a9b7f784a88b96bbb5922588ba037e6e80cf
pf-github-pl <pf.github@gmail.com>
2023-12-11 13:39:37 +0100

    change wording on the location form button

--- main/forms.py
+++ main/forms.py
@@ -12,4 +12,9 @@ class LocationForm(forms.Form):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = "post"
-        self.helper.add_input(Submit('submit', 'Submit', css_class="btn btn-primary rounded-pill w-100"))
+        self.helper.add_input(
+            Submit('submit',
+                   'Find food trucks nearby',
+                   css_class="btn btn-primary rounded-pill w-100"
+                   )
+        )
